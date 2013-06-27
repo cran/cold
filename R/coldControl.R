e440a7e9722f99bb3d29effd3d6738999ed35a44
M. Helena Gonçalves <mhgoncal@ualg.pt>
2013-06-27 00:00:00 +0000

    version 1.0-1-dup

--- R/coldControl.R
+++ R/coldControl.R
@@ -1,5 +1,5 @@
 
-coldControl<- function (maxit = 100, abstol = 1e-006, reltol = 1e-006)
+coldControl<- function (maxit = 100, abstol = -Inf, reltol = sqrt(.Machine$double.eps))
 {
     list(maxit =maxit, abstol =abstol , reltol =  reltol)
 }
